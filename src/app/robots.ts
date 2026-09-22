38a67aede8b49a708ed9d3ceddd0b6984b084087
Aldheeb11 <tariqalmarar@hotmail.com>
2026-09-22 17:39:06 +0400

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,8 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/', disallow: ['/api/'] },
+    sitemap: 'https://nudra-psi.vercel.app/sitemap.xml',
+  }
+}
